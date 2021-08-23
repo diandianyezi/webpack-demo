69d15e54afe35a4e57909351a385b2253736eba7
zhangting5 <zhangting5@xiaomi.com>
2021-08-24 07:35:44 +0800

    init

---
+++ webpack初体验/build/main.js
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ (() => {
+
+eval("/**\n * index.js 入口起点文件\n * \n * 1.运行指令\n * 开发环境 webpack ./src/index.js -o ./build --mode=development\n * \n * 生产环境 \n */\n\nfunction add(x,y) {\n    return x + y\n}\n\nconsole.info(add(1, 2));\n\n//# sourceURL=webpack://webpack-test/./src/index.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/index.js"]();
+/******/ 	
+/******/ })()
+;
---
+++ webpack初体验/src/index.js
@@ -0,0 +1,15 @@
+/**
+ * index.js 入口起点文件
+ * 
+ * 1.运行指令
+ * 开发环境 webpack ./src/index.js -o ./build --mode=development
+ * 开发环境 webpack ./src/index.js -o ./dist --mode=production
+ * 
+ * 生产环境 
+ */
+
+function add(x,y) {
+    return x + y
+}
+
+console.info(add(1, 2));
